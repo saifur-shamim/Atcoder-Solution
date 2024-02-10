2761ccf568426c7ccd30c1f0fb7c4decda777a1e
saifur-shamim <saifurshamim150@gmail.com>
2024-02-10 20:02:30 +0600

    Atcoder Beginner Contest 340

---
+++ B-340 D Super Takahashi Bros .cpp
@@ -0,0 +1,152 @@
+///Bismillahir Rahmanir Rahim
+///Happy Coding
+
+/*
+
+     * * * *             *              *  * * * * * *   *            *   * * * * *
+    *                   * *             *  *             *            *   *         *
+    *                  *   *            *  *             *            *   *          *
+    *                 *     *           *  *             *            *   *           *
+    *                *       *          *  *             *            *   *           *
+     *              *         *         *  * * * * * *   *            *   *          *
+       *           *           *        *  *             *            *   * * * * * *
+         *        *             *       *  *             *            *   *  *
+          *      * * * * * * * * *      *  *             *            *   *    *
+           *    *                 *     *  *             *            *   *      *
+           *   *                   *    *  *             *            *   *        *
+           *  *                     *   *  *              *          *    *          *
+    * * * *  *                       *  *  *                * * * * *     *            *
+
+
+*/
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+void faltu ()
+{
+    cerr << endl;
+}
+template < typename T, typename ... hello>void faltu( T arg, const hello &... rest)
+{
+    cerr << arg << ' ';
+    faltu(rest...);
+}
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+#define ordered_set tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>
+
+///order_of_key (val) : Number of items strictly smaller than val.
+///find_by_order(x) : x-th element in a set (zero based index).
+
+typedef  long long ll;
+typedef  long int li;
+typedef  unsigned long long ull;
+typedef  double dl;
+typedef  vector<int> vi;
+typedef  vector<ll> vl;
+typedef  pair<int,int> pii;
+typedef  pair<ll,ll> pll;
+typedef  vector<pii>vpi;
+typedef  vector<pll> vpl;
+
+#define     pb        push_back
+#define     dlt       pop_back
+#define     MP        make_pair
+#define     F         first
+#define     S         second
+#define     sz(x)     (int)x.size()
+#define     all(a)    (a).begin(),(a).end()
+#define     faster    ios_base::sync_with_stdio(0);cout.tie(0);cin.tie(0);
+#define     endl      '\n'
+#define     mem(a,b)  memset(a,b,sizeof(a))
+#define     mod       1000000007
+#define     gcd(a,b)  __gcd(a,b)
+#define     pf        printf
+#define     YES       cout<<"YES\n"
+#define     Yes       cout<<"Yes\n"
+#define     NO        cout<<"NO\n"
+#define     No        cout<<"No\n"
+#define     nl        cout<<endl
+#define     mone      cout<<"-1\n"
+#define    For(i,a,b)  for(i = a; i <= b; i++)
+#define    Forr(i,a,b)  for(i = a; i >= b; i--)
+
+// int dx[] = { -1 , 1 ,  0  , 0 , -1  , -1  ,  1  , 1  };
+// int dy[] = { 0  , 0 , -1  , 1 , -1  ,  1  , -1  , 1  };
+
+//            { U  , D ,  L  , R , UL  , UR  , DL  , DR }
+
+const dl PI=acos(-1);
+const int inf=2e9;
+
+
+const ll infll=900000000000000000;
+const int mx=2e5+6;
+
+ll n,m;
+vpl adj[mx];
+ll dis[mx];
+
+void dijkstra(ll s,ll n)
+{
+    for(ll i=0; i<=n; i++)
+    {
+        dis[i]=infll;
+    }
+
+    dis[s]=0;
+
+    priority_queue<pll,vpl,greater<pll>> pq;
+
+    pq.push({0,s});
+
+    while(!pq.empty())
+    {
+        ll curd=pq.top().F;
+        ll node=pq.top().S;
+        pq.pop();
+        if(dis[node]<curd) continue;
+        for(auto it:adj[node])
+        {
+            ll child=it.F;
+
+            ll w=it.S;
+
+            if(curd+w<dis[child])
+            {
+                dis[child]=curd+w;
+                pq.push({dis[child],child});
+            }
+        }
+    }
+}
+int main()
+{
+    cin>>n;
+
+    ll i;
+    for(i=1; i<=n-1; i++)
+    {
+        ll u,v,w;
+        cin>>u>>v>>w;
+
+
+        adj[i].pb({i+1,u});
+
+        adj[i].pb({w,v});
+    }
+
+
+    dijkstra(1,n);
+
+    cout<<dis[n];nl;
+    return 0;
+}
